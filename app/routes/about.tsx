046ed6df4ab988dc0515fd9fedcc2d0ff4f77d73
WyahidBadriPersonal <NotWyahid@gmail.com>
2025-01-27 15:32:31 -0500

    countries, country and about

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,27 @@
 export default function About() {
-    return <div>About</div>;
+  return (
+    <div className="py-16 bg-white">
+      <div className="container mx-auto px-4 max-w-4xl">
+        <h1 className="text-4xl font-extrabold text-gray-900 mb-6 text-center">About Country explorer</h1>
+        <p className="text-xl text-gray-700 leading-relaxed mb-4">
+          This project utalizes the REST API provided by 
+          <a
+            className="underline text-indigo-400 font-semibold"
+            href="https://restcountries.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            restcountries.com
+          </a>
+          to query information about countries around the world by name and
+          region.
+        </p>
+        <p className="text-lg text-gray-700 leading-relaxed mb-4">
+          The goal was build buld a responsive and interactive web application
+          that allows users to search for information about countries around the
+          world. Using Reacr Router 7, TailwindCSS and TypeScript
+        </p>
+      </div>
+    </div>
+  );
 }
--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,10 +1,75 @@
+import { Link } from "react-router";
+import type { Route } from "./+types/countries";
+import { useState } from "react";
 
 export async function clientLoader() {
-    const res = await fetch("https://restcountries.com/v3.1/all");
-    const data = await res.json();
-    return data;
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  return data;
 }
 
-export default function Countries() {
-    return <div>Countries</div>;
+export default function Countries({ loaderData }: Route.ComponentProps) {
+  const [search, setSearch] = useState<string>("");
+  const [region, setRegion] = useState<string>("");
+
+  const filteredData = loaderData.filter((country: any) => {
+    const matchesRegion = !region || country.region.toLowerCase() === region.toLocaleLowerCase();
+    const matchesSearch =
+      !search ||
+      country.name.common.toLowerCase().includes(search.toLowerCase());
+    return matchesSearch && matchesRegion;
+  });
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6 text-gray-900">Countries</h1>
+      <div className="flex flex-col sm:flex-row gap-4 mb-6">
+        <input
+          type="text"
+          placeholder="Search Country"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-indigo-500"
+        />
+        <select
+          value={region}
+          onChange={(event) => setRegion(event.target.value)}
+          className="border border-gray-300 rounded px-3 py-2 w-full sm:w-1/2 focus:outline-none focus:border-indigo-500"
+        >
+          <option value="">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Antarctic">Antarctic</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      {filteredData.length === 0 ? (
+        <div className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 max-w-150" role="alert">
+        <span className="font-medium">Warning alert!</span> No Countries found. Change region and try again.
+      </div>
+      ) : (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {filteredData.map((country: any) => (
+            <li
+              key={country.cca3}
+              className="bg-white border border-gray-200 rounded-xl p-4 shadow hover:shadow-lg transition"
+            >
+              <Link
+                to={`/countries/${country.name.common}`}
+                className="text-indigo-600 hover:underline text-lg font-semibold"
+              >
+                {country.name.common}
+              </Link>
+              <div className="text-gray-600 text-sm mt-1">
+                Region: {country.region} <br />
+                Population: {country.population.toLocaleString()}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
